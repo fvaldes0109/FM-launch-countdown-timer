ecc8f9fed44f99a89d59a75d3635a5426fe42e42
fvaldes0109 <fvaldes0109@gmail.com>
2023-02-20 00:06:57 -0500

    Setup React app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      We're launching soon
+  
+      Days
+      Hours
+      Minutes
+      Seconds
+      
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+        Coded by <a href="https://www.linkedin.com/in/fernando-vald%C3%A9s-garc%C3%ADa-a46142221/">Fernando Valdés García</a>.
+  </div>
+    </div>
+  );
+}
+
+export default App;
