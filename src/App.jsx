40afdbe575ae9637a8912e9e7d1f214eecd8a391
fvaldes0109 <fvaldes0109@gmail.com>
2023-02-20 20:39:19 -0500

    React fixes and basic styles

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <main>
+          <div className="mountains-container"></div>
+          <h1>WE'RE LAUNCHING SOON</h1>
+        </main>
+        
+        <footer>
+          <div className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+            Coded by <a href="https://www.linkedin.com/in/fernando-vald%C3%A9s-garc%C3%ADa-a46142221/">Fernando Valdés García</a>.
+          </div>
+        </footer>
+      </>
+    );
+  }
+}
+
+export default App;
